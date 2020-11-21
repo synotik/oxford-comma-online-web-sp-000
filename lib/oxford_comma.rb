c2262631da960567c8022dfb014c3b0b58ef9a4c
Jeff Johnson <synotik@hotmail.com>
2020-11-21 07:47:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,11 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  elsif array.length == 3
+  else
     last = array.last
     array.pop
     string = array.join(", ")
     string << ", and #{last}"
     return string
-  else
-    array.join(", ")
   end
 end
